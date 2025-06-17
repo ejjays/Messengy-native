9454446f4edbeebdbba0ef2e2f3ae69bafae6599
Ejjay <ejalloso021@gmail.com>
2025-06-17 07:46:03 +0000

    running expo but still errors

---
+++ node_modules/stream-chat-react-native-core/src/utils/getTrimmedAttachmentTitle.ts
@@ -0,0 +1,13 @@
+import { lookup } from 'mime-types';
+
+export const getTrimmedAttachmentTitle = (title?: string) => {
+  if (!title) return '';
+
+  const mimeType = lookup(title);
+  if (mimeType) {
+    const lastIndexOfDot = title.lastIndexOf('.');
+    return title.length < 12 ? title : title.slice(0, 12) + '...' + title.slice(lastIndexOfDot);
+  } else {
+    return title;
+  }
+};
